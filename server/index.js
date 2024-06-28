68e490fb54df6a1757ac2654ea055f5e933e98ac
Anurag mishra <anuragmishra.08c@gmail.com>
2024-06-28 19:30:12 +0530

    changed some file paths

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-import {app} from "./app.js"
+import express from "express";
+import { config } from "dotenv";
+import paymentRoute from "./routes/paymentRoutes.js";
+import cors from "cors"
 import Razorpay from "razorpay"
 import { connectDB } from "./config/db.js";
+
+
+config({ path: "./config/config.env" })
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
@@ -9,7 +21,11 @@ export const instance = new Razorpay({
     key_secret: process.env.RAZORPAY_API_SECRET,
   });
 
+app.use("/api", paymentRoute)
 
+app.get("/api/getkey",(req,res)=>{
+    res.status(200).json({key:process.env.RAZORPAY_API_KEY})
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is working on ${process.env.PORT}`)
